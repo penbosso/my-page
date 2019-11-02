92aff7b1d1c276ebdbd3ad22a6c1f22d8cddf78f
penbosso <ckbosso@st.ug.edu.gh>
2019-11-02 18:25:46 +0000

    Node environment

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,35 @@
+import express from'express';
+import path from'path';
+import open from'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+/* eslint-disable no console */
+
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath:config.output.publicPath
+}));
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.get('/users', function(req, res) {
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName":"Aidy","email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Kwe","lastName":"Smith","email":"kwe@gmail.com"}
+  ]);
+});
+
+app.listen(port, function(err){
+  if(err){
+    console.log(err);
+  }
+  else{
+    open('http://localhost:'+ port);
+  }
+});
